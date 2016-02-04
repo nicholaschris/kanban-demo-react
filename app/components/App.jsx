3aa6ceb55f7fb7f2841329d53f2bf2add446459d
Nicholas <nicholaschristopherpringle@gmail.com>
2016-02-04 17:41:20 +0100

    Add drag and drop to Kanban

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,38 @@
+import AltContainer from 'alt-container';
 import React from 'react';
-import Note from './Note.jsx';
 
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
+
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+@DragDropContext(HTML5Backend)
 export default class App extends React.Component {
-  render() {
-    return <Note />;
-  }
+
+    render() {
+
+        return (
+            <div>
+                <button className="add-lane" onClick={this.addLane}>+ Add New Lane</button>
+
+                <AltContainer
+                  stores={[LaneStore]}
+                  inject={{
+                    lanes: () => LaneStore.getState().lanes || []
+                  }}
+                >
+                  <Lanes />
+                </AltContainer>
+
+            </div>
+        );
+
+    }
+
+    addLane() {
+        LaneActions.create({task: 'New lane'});
+    }
+
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,54 @@
 import React from 'react';
 
-export default () => <div>Learn Webpack and say hello world</div>;
+import {DragSource, DropTarget} from 'react-dnd';
+
+import ItemTypes from '../constants/itemTypes';
+
+const noteSource = {
+  beginDrag(props) {
+    return {
+      id: props.id
+    };
+  },
+  isDragging(props, monitor) {
+    return props.id === monitor.getItem().id;
+  }
+};
+const noteTarget = {
+    hover(targetProps, monitor) {
+      const targetId = targetProps.id;
+      const sourceProps = monitor.getItem();
+      const sourceId = sourceProps.id;
+
+      if(sourceId !== targetId) {
+        targetProps.onMove({sourceId, targetId});
+      }
+    }
+};
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging() // map isDragging() state to isDragging prop
+}))
+
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
+export default class Note extends React.Component {
+  render() {
+    const {connectDragSource, id, onMove, ...props} = this.props;
+
+    return connectDragSource(
+      <li {...props}>{props.children}</li>
+    );
+  }
+  render() {
+    const {connectDragSource, connectDropTarget, isDragging,
+      onMove, id, ...props} = this.props;
+
+    return connectDragSource(connectDropTarget(
+      <li style={{
+        opacity: isDragging ? 0 : 1
+      }} {...props}>{props.children}</li>
+    ));
+  }
+}
